5e16d6886374ffe23619115aedd7500de64a8f1a
priyank3487 <pikachu.sinchan.23450@gmail.com>
2021-12-21 18:30:47 +0530

    first commit

---
+++ vue reactivity/app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      debugger;
+      this.message = this.currentUserInput;
+      console.log('message');
+    },
+  },
+});
+
+app.mount('#app');
+
+
+
+
+
+// ................
+const data = {
+  message = 'hello'
+};
+const heandeler = {
+  set(target, key, value){
+    console.log(target);
+    console.log(key);
+    console.log(value);
+  }
+};
+const proxy = new Proxy(data, heandeler);
